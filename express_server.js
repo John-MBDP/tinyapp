60a42459233b271a13c0c188dff287996509077f
John <johnartuz@hotmail.com>
2021-12-06 18:00:42 -0500

    added to show the long version of the url from the request params

--- express_server.js
+++ express_server.js
@@ -9,7 +9,6 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -18,6 +17,12 @@ app.get("/urls.json", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 })
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  res.render("urls_show", templateVars);
+});
+
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
