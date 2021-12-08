e172307f80fa7ad3b965b031fcd1db3c1c39bffa
John <johnartuz@hotmail.com>
2021-12-07 20:58:57 -0500

    Implemented Log in and Logout Functionality via POST request

--- express_server.js
+++ express_server.js
@@ -3,6 +3,9 @@ const app = express();
 const PORT = 8080;
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
+const cookieParser = require("cookie-parser");
+const { request } = require("express");
+app.use(cookieParser())
 app.set("view engine", "ejs");
 
 //Generate a random string
@@ -26,7 +29,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase, username: req.cookies["username"]};
   res.render("urls_index", templateVars);
 });
 
@@ -38,6 +41,7 @@ app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
 });
+
 
 app.get("/u/:shortURL", (req, res) => {
   console.log(urlDatabase);
@@ -83,6 +87,20 @@ app.post("/urls/:shortURL", (req, res) => {
   urlDatabase[shortURL] = req.body.updatedURL;
   console.log(req.body.updatedURL);
   res.redirect("/urls");
+});
+
+//Handles Post to /login
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  res.cookie("username", username);
+  res.redirect("/urls");
+})
+
+//Clears cookies after user logsout
+app.post('/logout', (req, res) => {
+  const username = req.body.username
+  res.clearCookie('username', username);
+  res.redirect('/urls');
 });
 
 app.listen(PORT, () => {
